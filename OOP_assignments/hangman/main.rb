4a0dd513d52f52bf5f36090ae8151cb23aa83239
Jamie Le Tran <jamietran200@gmail.com>
2026-02-04 17:01:21 +1100

    Completed hangman game in ruby.

--- OOP_assignments/hangman/main.rb
+++ OOP_assignments/hangman/main.rb
@@ -10,6 +10,43 @@ random_index = rand(lines.length)
 random_word = lines[random_index]
 
 puts "The random index is: #{random_index}"
-puts "The random word is: #{random_element}"
+puts "The random word is: #{random_word}"
 
+lives = 7
+finished = false
+guessed = Array.new(random_word.length, "_")
+puts "HANGMAN"
+puts "Guess the word within 7 incorrect guesses."
+puts guessed.join
+turn = 1
+until finished
+  puts "Turn #{turn}"
+  puts guessed.join
+  print "Enter your guess: "
+  guess = gets.chomp
+  indices = Array.new()
+  random_word.each_char.with_index do |char, index|
+    indices << index if char == guess
+  end
 
+  indices.each do |i|
+    guessed[i] = guess
+  end
+
+  if indices.empty?
+    lives -= 1
+    puts "Wrong guess! Lives remaining: #{lives}"
+  end
+
+  if guessed.join == random_word
+    finished = true
+    puts "Congrats you finished the game, the word was #{random_word}"
+  end
+  
+  if lives == 0
+    finished = true
+    puts "Game over! The word was #{random_word}"
+  end
+
+  turn += 1
+end
